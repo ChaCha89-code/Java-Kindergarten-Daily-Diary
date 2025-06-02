ee703e2746ba89e61b4346b0540cb339d7000d17
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-06-02 15:56:32 +0900

    refactor: properties의 image path를 local로 수정함

--- src/main/java/com/diary/demo/repository/DiaryRepository.java
+++ src/main/java/com/diary/demo/repository/DiaryRepository.java
@@ -2,6 +2,8 @@ package com.diary.demo.repository;
 
 import com.diary.demo.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 }
